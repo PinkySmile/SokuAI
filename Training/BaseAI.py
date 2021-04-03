130fb76dfa865fcf59571adcac5f8b75581baaf8
Gegel85 <Gegel85@laposte.net>
2021-04-03 20:49:39 +0900

    Added missing files

---
+++ Training/BaseAI.py
@@ -0,0 +1,302 @@
+import random
+
+
+class BaseAI:
+    DOWN = 1
+    UP = -1
+    BACK = -1
+    FRONT = 1
+    NEUTRAL = 0
+    EMPTY = {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+    buffered_input = []
+    actions = [
+        "1a",
+        "2a",
+        "3a",
+        "4a",
+        "5a",
+        "6a",
+        "8a",
+        "66a",
+        "2b",
+        "3b",
+        "4b",
+        "5b",
+        "6b",
+        "66b",
+        "1c",
+        "2c",
+        "3c",
+        "4c",
+        "5c",
+        "6c",
+        "66c",
+        "236b",
+        "236c",
+        "623b",
+        "623c",
+        "214b",
+        "214c",
+        "421b",
+        "421c",
+        "22b",
+        "22c",
+        "nothing",
+        "forward",
+        "backward",
+        "block",
+        "forward_jump",
+        "backward_jump",
+        "neutral_jump",
+        "forward_highjump",
+        "backward_highjump",
+        "neutral_highjump",
+        "forward_dash",
+        "backward_dash",
+        "forward_fly",
+        "backward_fly",
+        "be1",
+        "be2",
+        "be4",
+        "be6",
+        "switch_to_card_1",
+        "switch_to_card_2",
+        "switch_to_card_3",
+        "switch_to_card_4",
+        "use_current_card"
+    ]
+    actions_buffers = {
+        "1a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN}
+        ],
+        "2a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN}
+        ],
+        "3a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN}
+        ],
+        "4a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "5a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+        ],
+        "6a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL}
+        ],
+        "8a": [
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+        ],
+        "66a": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": True, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "2b": [
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN}
+        ],
+        "3b": [
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN}
+        ],
+        "4b": [
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "5b": [
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+        ],
+        "6b": [
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL}
+        ],
+        "66b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "1c": [
+            {"A": True, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN}
+        ],
+        "2c": [
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN}
+        ],
+        "3c": [
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN}
+        ],
+        "4c": [
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "5c": [
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+        ],
+        "6c": [
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL}
+        ],
+        "66c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "236b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN},
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "236c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN},
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "623b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN},
+        ],
+        "623c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": FRONT, "V": DOWN},
+        ],
+        "214b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+        ],
+        "214c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+        ],
+        "421b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+        ],
+        "421c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+        ],
+        "22b": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            EMPTY,
+            {"A": False, "B": True, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+        ],
+        "22c": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            EMPTY,
+            {"A": False, "B": False, "C": True, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+        ],
+        "nothing": [EMPTY],
+        "forward": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL}
+        ],
+        "backward": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "block": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "forward_jump": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": UP}
+        ],
+        "backward_jump": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": UP}
+        ],
+        "neutral_jump": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": UP}
+        ],
+        "forward_highjump": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": FRONT, "V": UP}
+        ],
+        "backward_highjump": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": UP}
+        ],
+        "neutral_highjump": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": NEUTRAL, "V": UP}
+        ],
+        "forward_dash": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "backward_dash": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+        ],
+        "forward_fly": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL}
+        ],
+        "backward_fly": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL}
+        ],
+        "be1": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": DOWN},
+        ],
+        "be2": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": NEUTRAL, "V": DOWN},
+        ],
+        "be4": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": BACK, "V": NEUTRAL},
+        ],
+        "be6": [
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+            {"A": False, "B": False, "C": False, "D": True, "SC": False, "SW": False, "H": FRONT, "V": NEUTRAL},
+        ],
+        "switch_to_card_1": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+        ],
+        "switch_to_card_2": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+        ],
+        "switch_to_card_3": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+        ],
+        "switch_to_card_4": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+            EMPTY,
+            {"A": False, "B": False, "C": False, "D": False, "SC": False, "SW": True, "H": NEUTRAL, "V": NEUTRAL},
+        ],
+        "use_current_card": [
+            {"A": False, "B": False, "C": False, "D": False, "SC": True, "SW": False, "H": NEUTRAL, "V": NEUTRAL}
+        ]
+    }
+
+    def on_win(self):
+        pass
+
+    def on_lose(self):
+        pass
+
+    def on_timeout(self):
+        self.on_lose()
+
+    @staticmethod
+    def get_action(me, opponent, my_projectiles, opponent_projectiles, weather_infos):
+        return random.choice(BaseAI.actions)
+
+    def get_inputs(self, me, opponent, my_projectiles, opponent_projectiles, weather_infos):
+        if not len(self.buffered_input):
+            self.buffered_input = BaseAI.actions_buffers[
+                self.get_action(me, opponent, my_projectiles, opponent_projectiles, weather_infos)
+            ].copy()
+        return self.buffered_input.pop(0)
---
+++ Training/GameManager.py
@@ -0,0 +1,70 @@
+from Training import GameInstance, BaseAI
+import time
+import sys
+
+
+class GameManager:
+    game_instance: GameInstance.GameInstance = None
+    left_ai: BaseAI.BaseAI = None
+    right_ai: BaseAI.BaseAI = None
+
+    def __init__(self, game_path, port, ais, tps=60, has_display=True, has_sound=True, ini_path=None):
+        self.game_instance = GameInstance.GameInstance(game_path, ini_path, port)
+        self.left_ai = ais[0]
+        self.right_ai = ais[1]
+        self.game_instance.set_display_mode(has_display)
+        self.game_instance.set_game_speed(tps)
+        if not has_sound:
+            self.game_instance.set_game_volume(0, 0)
+
+    def start_game_sequence(self, stage, music, left_params, right_params):
+        while True:
+            try:
+                return self.game_instance.start_game({
+                    "stage": stage,
+                    "music": music,
+                    "left": left_params,
+                    "right": right_params,
+                })
+            except GameInstance.ProtocolError:
+                if sys.exc_info()[1].code != 12:
+                    raise
+                time.sleep(0.1)
+
+    def run_once(self, stage, music, left_params, right_params, frame_timout):
+        state = self.start_game_sequence(stage, music, left_params, right_params)
+        while True:
+            try:
+                frame_timout -= 1
+                #if frame_timout == 17500:
+                #    self.game_instance.set_health(500, 500)
+                #    self.game_instance.set_weather(13, 999, False)
+                #    self.game_instance.set_positions({"x": 40, "y": 0}, {"x": 1240, "y": 0})
+                if frame_timout <= 0:
+                    self.game_instance.end_game()
+                state = self.game_instance.tick({
+                    "left": self.left_ai.get_inputs(
+                        state["left"], state["right"], state["weather"], state["left_objs"], state["right_objs"]
+                    ),
+                    "right": self.left_ai.get_inputs(
+                        state["right"], state["left"], state["weather"], state["right_objs"], state["left_objs"]
+                    ),
+                })
+            except GameInstance.GameEndedException:
+                winner = sys.exc_info()[1].winner
+                if winner == 1:
+                    self.left_ai.on_win()
+                    self.right_ai.on_lose()
+                elif winner == 2:
+                    self.right_ai.on_win()
+                    self.left_ai.on_lose()
+                else:
+                    self.left_ai.on_timeout()
+                    self.right_ai.on_timeout()
+                return winner
+
+    def run(self, stage, music, left_params, right_params, nb, frame_timout):
+        result = []
+        for i in range(nb):
+            result.append(self.run_once(stage, music, left_params, right_params, frame_timout))
+        return result
